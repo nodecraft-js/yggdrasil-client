132e29909c1ba0cb778342436793c7b861d5e246
Jason Kwok <JasonHK@users.noreply.github.com>
2020-01-03 16:05:12 +0800

    chore: added index files for `/errors`, `/utilities` and `/yggdrasil`

---
+++ src/errors.ts
@@ -0,0 +1,14 @@
+"use strict";
+
+import { default as UnknownError } from "./errors/unknown";
+import { default as YggdrasilError } from "./errors/yggdrasil";
+
+export {
+    UnknownError,
+    YggdrasilError,
+};
+
+export default {
+    UnknownError,
+    YggdrasilError,
+};
