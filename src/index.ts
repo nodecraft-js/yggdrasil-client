f78d8a0a07dacb51f5d835e7cc3727163a2286e1
Jason Kwok <JasonHK@users.noreply.github.com>
2020-01-03 16:08:03 +0800

    chore: added `index.ts`

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+"use strict";
+
+import { default as YggdrasilClient } from "./client";
+import { default as RawYggdrasilClient } from "./raw-client";
+
+export {
+    YggdrasilClient,
+    RawYggdrasilClient,
+};
